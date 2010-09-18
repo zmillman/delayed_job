3318d67133b5d0b6fb9bf9a303a892604eedfba2
Brandon Keepers <brandon@collectiveidea.com>
2010-09-18 02:10:25 -0400

    Remove Worker#name=

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -21,9 +21,6 @@ module Delayed
       warn "[DEPRECATED] Delayed::Worker.logger= no longer exists. Use the :log option with #initialize"
     end
 
-    # name_prefix is ignored if name is set directly
-    attr_accessor :name_prefix
-
     cattr_reader :backend
     attr_reader :worker_id, :options
 
@@ -41,24 +38,18 @@ module Delayed
     end
 
     def initialize(worker_id, options={})
+      @worker_id = worker_id
       @options = options
       self.class.min_priority = options[:min_priority] if options.has_key?(:min_priority)
       self.class.max_priority = options[:max_priority] if options.has_key?(:max_priority)
     end
 
-    # Every worker has a unique name which by default is the pid of the process. There are some
-    # advantages to overriding this with something which survives worker retarts:  Workers can#
-    # safely resume working on tasks which are locked by themselves. The worker will assume that
-    # it crashed before.
     def name
-      return @name unless @name.nil?
-      "#{@name_prefix}host:#{Socket.gethostname} pid:#{Process.pid}" rescue "#{@name_prefix}pid:#{Process.pid}"
+      @name ||= "host:#{Socket.gethostname rescue 'unknown'} pid:#{$$} worker:#{@worker_id}"
     end
 
-    # Sets the name of the worker.
-    # Setting the name to nil will reset the default worker name
     def name=(val)
-      @name = val
+      warn "[DEPRECATED] worker name can no longer be set"
     end
 
     def run(job)
